b6d18259da72b76b300643e8c2804a1d469e5a45
HelTen-tom <142582424+HelTen-tom@users.noreply.github.com>
2024-03-22 16:28:59 +0200

    add proj

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { createBtn } from "./js/createBtn.js";
 import { values } from "./refs.js";
-
+const listEl = document.querySelector("list");
 document.addEventListener("DOMContentLoaded", function () {
   const buttonsContainer = document.querySelector("#list");
 
@@ -10,29 +10,27 @@ document.addEventListener("DOMContentLoaded", function () {
   const buttons = values.map((value) => {
     const button = createBtn(value, selectedButtons.includes(value.id));
     return button;
-    // if (selectedButtons.includes(value.id)) {
-    //   button.classList.add("selected");
-    // }
   });
-  console.log(JSON.stringify(buttons));
-  console.log(buttons.join(""));
   buttonsContainer.innerHTML = buttons.join("");
 });
+listEl.addEventListener("click", (e) => {
+    console.log(e.target);
+//   if (button.classList.contains("selected")) {
+//     button.classList.remove("selected");
+    
+//     const index = selectedButtons.indexOf(value.id);
+//     if (index > -1) {
+//       selectedButtons.splice(index, 1);
+//     }
+//   } else {
+//     button.classList.add("selected");
+    
+//     selectedButtons.push(value.id);
+//   }
+  
+//   localStorage.setItem("name-buttons", JSON.stringify(selectedButtons));
+});
+
 
 // button.addEventListener("click", function () {
-//     if (button.classList.contains("selected")) {
-//       button.classList.remove("selected");
-//       // Remove button ID from selectedButtons array
-//       const index = selectedButtons.indexOf(value.id);
-//       if (index > -1) {
-//         selectedButtons.splice(index, 1);
-//       }
-//     } else {
-//       button.classList.add("selected");
-//       // Add button ID to selectedButtons array
-//       selectedButtons.push(value.id);
-//     }
-//     // Save selectedButtons array to local storage
-//     localStorage.setItem("name-buttons", JSON.stringify(selectedButtons));
-//   });
-// buttonsContainer.appendChild(button);
+//});
